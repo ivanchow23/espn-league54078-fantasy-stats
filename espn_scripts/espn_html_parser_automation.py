3eceb3f8a98a9473bbb842f75376e2c64039bafe
Ivan Chow <ivanchow23@hotmail.com>
2021-04-12 22:00:07 -0600

    [ESPN] Automation moves output files into folders
    Update automation script to move output files into folders based on
    file extension for organization purposes. This also solves the issue
    where the Excel file output exists in the directory and has sheets
    get appended to it every run.

--- espn_scripts/espn_html_parser_automation.py
+++ espn_scripts/espn_html_parser_automation.py
@@ -23,6 +23,35 @@ def _find_files_recursive(root_folder, file_pattern_re):
                 file_paths_list.append(os.path.join(root, f))
     return file_paths_list
 
+def _find_folders_with_html(root_folder):
+    """ Recursively finds all folders that contain at least one HTML file. """
+    folder_paths_list = []
+    for root, _, files in os.walk(root_folder):
+        for f in files:
+            if f.endswith(".html"):
+                folder_paths_list.append(root)
+                break
+    return folder_paths_list
+
+def _move_files(src_folder, dest_folder, file_ext):
+    """ Helper function that moves all files with given file extension from source to destination folder. """
+    # Find files with given file extension in source folder
+    print(f"Moving {file_ext} files from {src_folder} to {dest_folder}")
+    files_to_move = []
+    for f in os.listdir(src_folder):
+        if f.endswith(file_ext):
+            files_to_move.append(f)
+
+    # Create destination folder if it doesn't exist
+    if not os.path.exists(dest_folder):
+        os.mkdir(dest_folder)
+
+    # Move all files to destination folder - overwrite if it already exists
+    for f in files_to_move:
+        src_file_path = os.path.join(src_folder, f)
+        dest_file_path = os.path.join(dest_folder, f)
+        os.replace(src_file_path, dest_file_path)
+
 if __name__ == "__main__":
     """ Main function. """
     # Read arguments
@@ -63,3 +92,14 @@ if __name__ == "__main__":
         file_paths = _find_files_recursive(root_dir, espn_utils.FILE_NAME_RE_FORMAT_LEAGUE_ROSTERS)
         espn_league_rosters_html_parser.to_csv(file_paths)
         espn_league_rosters_html_parser.to_excel(file_paths)
+
+    # Move files into folders
+    # Find folder paths that contain HTML files because that is likely where all the output files are to move
+    print("\n--------------------------------------------------- Moving output files -------------------------------------------------")
+    folder_paths_with_html = _find_folders_with_html(root_dir)
+    for folder_path in folder_paths_with_html:
+        _move_files(folder_path, os.path.join(folder_path, "csv"), ".csv")
+        _move_files(folder_path, os.path.join(folder_path, "excel"), ".xlsx")
+
+    # Done!
+    print("Done.")
