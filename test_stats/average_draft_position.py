4fdb5630948ef67d5d5a777457198941f301002e
Ivan Chow <ivanchow23@hotmail.com>
2021-09-11 13:05:39 -0600

    Update existing test stats
    Updated test stat for average draft position. Takes in path to
    root ESPN data folder. Uses ESPN loader to load validated data.
    Removed test stat that finds players drafted on final roster.
    Turns out it uses parsed league roster information which has
    been removed (might need a TODO to bring it back in the future).

--- test_stats/average_draft_position.py
+++ test_stats/average_draft_position.py
@@ -3,35 +3,37 @@
 import argparse
 import os
 import pandas as pd
-import re
+import sys
 
-DRAFT_RECAP_CSV_FILE_NAME_RE = r"Draft Recap - [\W\w]+.csv"
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+sys.path.insert(1, os.path.join(SCRIPT_DIR, "..", "espn_scripts"))
+from espn_loader import EspnLoader
 
 if __name__ == "__main__":
-    """ Usage: Enter folder containing folders of data from each season. """
     arg_parser = argparse.ArgumentParser()
-    arg_parser.add_argument('-d', required=True, help="Input directory containing folders of data of each season.")
+    arg_parser.add_argument('--espn', required=True, help="Root path of parsed ESPN data from ESPN scripts.")
     args = arg_parser.parse_args()
-    root_folder_path = args.d
+    espn_root_path = args.espn
 
-    # Find all draft recap files
-    file_paths_list = []
-    for root, dirs, files in os.walk(root_folder_path):
-        for f in files:
-            if re.match(DRAFT_RECAP_CSV_FILE_NAME_RE, f):
-                file_paths_list.append(os.path.join(root, f))
+    # Initialize
+    espn_loader = EspnLoader(espn_root_path)
 
-    # Read in files and combine to dataframe
+    # Get all seasons in root folder and combine into single dataframe
     combined_df = pd.DataFrame()
-    for file_path in file_paths_list:
-        file_df = pd.read_csv(file_path)
-        combined_df = pd.concat([combined_df, file_df], axis=0, ignore_index=True)
+    for season_string in os.listdir(espn_root_path):
+        # Load draft information
+        draft_df = espn_loader.load_draft_recap_data(season_string)
+        if draft_df is None:
+            continue
+        combined_df = pd.concat([combined_df, draft_df], ignore_index=True)
 
-    grouped_df = combined_df.groupby(['Player'])['Draft Number'].agg(['count', 'mean', 'min', 'max']).round(0)
-    grouped_df = grouped_df.rename(columns={'count': '# Times Drafted',
-                                            'mean': 'Average Draft Position',
+    # Operate on data
+    grouped_df = combined_df.groupby(['Player'])['Draft Number'].agg(['mean', 'min', 'max', 'count']).round(0)
+    grouped_df = grouped_df.rename(columns={'mean': 'Average Draft Position',
                                             'min': 'Highest Position',
-                                            'max': 'Lowest Position'})
+                                            'max': 'Lowest Position',
+                                            'count': '# Times Drafted'})
 
     grouped_df = grouped_df.sort_values(by='Average Draft Position')
     grouped_df.to_csv("avg_draft_pos.csv")
