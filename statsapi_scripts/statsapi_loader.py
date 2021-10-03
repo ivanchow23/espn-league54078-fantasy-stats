2d1717f10064a7faa16f3bc49bdd6ca886ff2be7
Ivan Chow <ivanchow23@hotmail.com>
2021-10-03 00:17:51 -0600

    [STATSAPI] Add loader utility
    Added statsapi loader utility with unit tests. Similar and
    adapted from the ESPN loader. Added a single API to start
    to retrieve player information dictionaries from downloaded
    JSON files. Utility object expects and reads in the player
    and team map files that are generated from the downloader
    script.

---
+++ statsapi_scripts/statsapi_loader.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+""" Utility to load statsapi downloaded data. """
+import csv
+import json
+import os
+import statsapi_logger
+
+logger = statsapi_logger.logger()
+
+class StatsapiLoader():
+    """ Holds a reference to the root statsapi data folder and provides
+        APIs to load data from it. This is a counterpart to the statsapi
+        downloader script. Assumes the general data folder structure:
+
+        root_folder
+          players
+          teams
+          players_id_map
+          teams_id_map
+          20192020
+            - team_rosters
+            - etc.
+          20202021
+            - team_rosters
+            - etc.
+    """
+    def __init__(self, root_folder_path):
+        """ Constructor. Takes in path to root data folder. """
+        self._root_folder_path = root_folder_path
+        self._players_map_dicts = self._read_csv_as_dicts(os.path.join(root_folder_path, "players_id_map.csv"))
+        self._teams_map_dicts = self._read_csv_as_dicts(os.path.join(root_folder_path, "teams_id_map.csv"))
+
+    def load_player_dict(self, player_name, team=None, season_string=None):
+        """ Loads data about the given player as a dictionary.
+            Optional parameters are to be more specific in the search
+            in case there are multiple players with the same name. """
+        id = self._get_player_id(player_name, team, season_string)
+        if id == -1:
+            return None
+
+        return self._load_json(os.path.join(self._root_folder_path, "players", f"player{id}.json"))
+
+    def _load_json(self, file_path):
+        """ Helper function to load a JSON file.
+            Returns None on failure. """
+        if not os.path.exists(file_path):
+            logger.warning(f"Cannot load from {file_path}")
+            return None
+
+        return json.load(open(file_path))
+
+    def _get_player_id(self, player_name, team=None, season_string=None):
+        """ Returns the player ID from the player map file. """
+        if self._players_map_dicts is None:
+            return -1
+
+        for d in self._players_map_dicts:
+            ret = False
+            if d['Player'] == player_name:
+                if team is None and season_string is None:
+                    return int(d['id'])
+                elif team is not None and season_string is None:
+                    if d['Team'] == team:
+                        return int(d['id'])
+                elif team is None and season_string is not None:
+                    if d['Season'] == season_string:
+                        return int(d['id'])
+                else:
+                    if d['Team'] == team and d['Season'] == season_string:
+                        return int(d['id'])
+        return -1
+
+    def _read_csv_as_dicts(self, file_path):
+        """ Reads a CSV file as a list of dictionaries.
+            Returns None on failure. """
+        if not self._check_path(file_path):
+            logger.warning(f"Cannot open: {file_path}")
+            return None
+
+        with open(file_path, 'r', encoding='utf-8') as csv_file:
+            dict_reader = csv.DictReader(csv_file)
+            return list(dict_reader)
+
+    def _check_path(self, path):
+        """ Helper function to test if a path is valid or exists. """
+        if not isinstance(path, str) or not os.path.exists(path):
+            return False
+        return True
+
---
+++ tests/test_statsapi_loader.py
@@ -0,0 +1,161 @@
+#!/usr/bin/env python
+import csv
+import json
+import os
+import shutil
+import unittest
+
+SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
+
+import sys
+sys.path.insert(1, os.path.join(SCRIPT_DIR, "..", "statsapi_scripts"))
+from statsapi_loader import StatsapiLoader
+
+class TestStatsapiLoader(unittest.TestCase):
+    def setUp(self):
+        """ Set-up required items. """
+        self._test_folder = os.path.join(SCRIPT_DIR, "test_statsapi_loader_folder")
+        os.makedirs(self._test_folder, exist_ok=True)
+
+    def test_load_player_dict(self):
+        """ Test loading player information as a dictionary. """
+        test_folder = os.path.join(self._test_folder, "test_load_player_dict")
+        self._generate_typical_test_folder_structure(test_folder)
+        statsapi = StatsapiLoader(test_folder)
+
+        # Test typical case - player name only
+        expected_data = {'Player Name': "Connor McDavid", 'Birth Country': "CAN"}
+        actual_data = statsapi.load_player_dict("Connor McDavid")
+        self.assertEqual(expected_data, actual_data)
+
+        # Test typical case - player name and team name
+        expected_data = {'Player Name': "Connor McDavid", 'Birth Country': "CAN"}
+        actual_data = statsapi.load_player_dict("Connor McDavid", team="EDM")
+        self.assertEqual(expected_data, actual_data)
+
+        # Test typical case - player name and season
+        expected_data = {'Player Name': "Connor McDavid", 'Birth Country': "CAN"}
+        actual_data = statsapi.load_player_dict("Connor McDavid", season_string="20202021")
+        self.assertEqual(expected_data, actual_data)
+
+        # Test typical case - player name, team, and season
+        expected_data = {'Player Name': "Connor McDavid", 'Birth Country': "CAN"}
+        actual_data = statsapi.load_player_dict("Connor McDavid", team="EDM", season_string="20202021")
+        self.assertEqual(expected_data, actual_data)
+
+        # Test typical case - with utf-8 encoded string
+        expected_data = {'Player Name': "Alexis Lafrenière", 'Birth Country': "CAN"}
+        actual_data = statsapi.load_player_dict("Alexis Lafrenière")
+        self.assertEqual(expected_data, actual_data)
+
+        # Test unknown player
+        self.assertIsNone(statsapi.load_player_dict("Sidney Crosby"))
+
+        # Test unknown team
+        self.assertIsNone(statsapi.load_player_dict("Connor McDavid", team="CGY"))
+
+        # Test unknown season
+        self.assertIsNone(statsapi.load_player_dict("Connor McDavid", season_string="19901991"))
+
+        # Test unknown team and season
+        self.assertIsNone(statsapi.load_player_dict("Connor McDavid", team="CGY", season_string="19901991"))
+
+        # Test known team but unknown season
+        self.assertIsNone(statsapi.load_player_dict("Connor McDavid", team="EDM", season_string="19901991"))
+
+        # Test known season but unknown team
+        self.assertIsNone(statsapi.load_player_dict("Connor McDavid", team="CGY", season_string="20202021"))
+
+    def test__get_player_id(self):
+        """ Test helper function to get player ID from map file. """
+        test_folder = os.path.join(self._test_folder, "test__get_player_id")
+        self._generate_typical_test_folder_structure(test_folder)
+        statsapi = StatsapiLoader(test_folder)
+
+        # Test typical case - player name only
+        expected_id = 8478402
+        actual_id = statsapi._get_player_id("Connor McDavid")
+        self.assertEqual(expected_id, actual_id)
+
+        # Test typical case - player name and team name
+        expected_id = 8478402
+        actual_id = statsapi._get_player_id("Connor McDavid", team="EDM")
+        self.assertEqual(expected_id, actual_id)
+
+        # Test typical case - player name and season
+        expected_id = 8478402
+        actual_id = statsapi._get_player_id("Connor McDavid", season_string="20202021")
+        self.assertEqual(expected_id, actual_id)
+
+        # Test typical case - player name, team, and season
+        expected_id = 8478402
+        actual_id = statsapi._get_player_id("Connor McDavid", team="EDM", season_string="20202021")
+        self.assertEqual(expected_id, actual_id)
+
+        # Test typical case - with utf-8 encoded string
+        expected_id = 8482109
+        actual_id = statsapi._get_player_id("Alexis Lafrenière")
+        self.assertEqual(expected_id, actual_id)
+
+        # Test unknown player
+        expected_id = -1
+        actual_id = statsapi._get_player_id("Sidney Crosby")
+        self.assertEqual(expected_id, actual_id)
+
+        # Test unknown team
+        expected_id = -1
+        actual_id = statsapi._get_player_id("Connor McDavid", team="CGY")
+        self.assertEqual(expected_id, actual_id)
+
+        # Test unknown season
+        expected_id = -1
+        actual_id = statsapi._get_player_id("Connor McDavid", season_string="19901991")
+        self.assertEqual(expected_id, actual_id)
+
+        # Test unknown team and season
+        expected_id = -1
+        actual_id = statsapi._get_player_id("Connor McDavid", team="CGY", season_string="19901991")
+        self.assertEqual(expected_id, actual_id)
+
+        # Test known team but unknown season
+        expected_id = -1
+        actual_id = statsapi._get_player_id("Connor McDavid", team="EDM", season_string="19901991")
+        self.assertEqual(expected_id, actual_id)
+
+        # Test known season but unknown team
+        expected_id = -1
+        actual_id = statsapi._get_player_id("Connor McDavid", team="CGY", season_string="20202021")
+        self.assertEqual(expected_id, actual_id)
+
+    def _generate_typical_test_folder_structure(self, root_path):
+        """ Helper function to generate a test folder structure similar
+            to what the statsapi downloader module would generate. """
+        os.makedirs(os.path.join(root_path, "players"), exist_ok=True)
+        os.makedirs(os.path.join(root_path, "teams"), exist_ok=True)
+        os.makedirs(os.path.join(root_path, "20202021"), exist_ok=True)
+        os.makedirs(os.path.join(root_path, "20202021"), exist_ok=True)
+
+        players_map_dicts = [{'Player': "Connor McDavid", 'Team': "EDM", 'Season': "20202021", 'id': 8478402},
+                             {'Player': "Alexis Lafrenière", 'Team': "NYR", 'Season': "20202021", 'id': 8482109}]
+        self._create_csv_file(os.path.join(root_path, "players_id_map.csv"), players_map_dicts)
+
+        teams_map_dicts = [{'id': 3, 'name': "New York Rangers", 'abbreviation': "NYR"},
+                           {'id': 22, 'name': "Edmonton Oilers", 'abbreviation': "EDM"}]
+        self._create_csv_file(os.path.join(root_path, "teams_id_map.csv"), teams_map_dicts)
+
+        test_player_dict = {'Player Name': "Connor McDavid", 'Birth Country': "CAN"}
+        json.dump(test_player_dict, open(os.path.join(os.path.join(root_path, "players", "player8478402.json")), 'w'))
+
+        test_player_dict = {'Player Name': "Alexis Lafrenière", 'Birth Country': "CAN"}
+        json.dump(test_player_dict, open(os.path.join(os.path.join(root_path, "players", "player8482109.json")), 'w'))
+
+    def _create_csv_file(self, file_path, data_dicts):
+        """ Helper function to create a CSV file. """
+        with open(file_path, 'w', encoding='utf-8') as csv_file:
+            dict_writer = csv.DictWriter(csv_file, fieldnames=data_dicts[0].keys())
+            dict_writer.writeheader()
+            dict_writer.writerows(data_dicts)
+
+    def tearDown(self):
+        """ Remove any items. """
+        shutil.rmtree(self._test_folder)
