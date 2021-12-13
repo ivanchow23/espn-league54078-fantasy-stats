e639622ede1537b83ff65d2c9e3c0c9abf264512
Ivan Chow <ivanchow23@hotmail.com>
2021-12-12 19:56:47 -0700

    [STATSAPI] Add APIs for asynchronous requests
    Added asynchronous load and save functionality to allow
    loading and saving data from a list of URLs.

--- statsapi_scripts/statsapi_utils.py
+++ statsapi_scripts/statsapi_utils.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 """ Common utilities file used for retrieving data from statsapi. """
+import aiohttp
+import asyncio
 import os
 import json
 import requests
 import statsapi_logger
+import sys
 
 URL_STRING = "https://statsapi.web.nhl.com"
 API_ENDPOINT_PREFIX = "/api/v1"
@@ -33,6 +36,49 @@ def save_json_from_url(url, out_file_path):
         json.dump(json_data, out_file)
 
     return True
+
+def load_jsons_from_urls_async(url_list):
+    """ Loads JSON data from the given list of URLs asynchronously.
+        Returns a list of dictionaries. """
+    # Set policy on Windows and Python 3.8+ to work around runtime exception:
+    # https://github.com/encode/httpx/issues/914#issuecomment-622586610
+    if (sys.version_info[0] == 3 and
+        sys.version_info[1] >= 8 and
+        sys.platform.startswith('win')):
+        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
+    return asyncio.run(_load_jsons_async(url_list))
+
+def save_jsons_from_urls_async(url_file_path_dict_list):
+    """ Saves JSON data from the given URLs and corresponding file output
+        path asynchronously. Input is a list of dictionaries in the form:
+        {'url': <url>, 'out_file_path': <output file path>}
+
+        Note: The requests each URL links is ran asynchronously. Saving
+        to JSON files are not. """
+    # Input is a dictionary to enforce that every URL link has a
+    # corresponding file output path. Unpack into individual lists here
+    # to take advantage of requesting JSON data asynchronously before
+    # writing to file.
+    url_list = []
+    file_path_list = []
+    for d in url_file_path_dict_list:
+        url_list.append(d['url'])
+        file_path_list.append(d['out_file_path'])
+
+    # Set policy on Windows and Python 3.8+ to work around runtime exception:
+    # https://github.com/encode/httpx/issues/914#issuecomment-622586610
+    if (sys.version_info[0] == 3 and
+        sys.version_info[1] >= 8 and
+        sys.platform.startswith('win')):
+        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
+    json_data_list = asyncio.run(_load_jsons_async(url_list))
+    for json_data, file_path in zip(json_data_list, file_path_list):
+        with open(file_path, 'w') as out_file:
+            json.dump(json_data, out_file)
+
+    return len(json_data_list)
 
 def get_full_url(endpoint):
     """ Helper function that retrieves the full statsapi URL given the endpoint. """
@@ -77,3 +123,28 @@ def _load_json(url):
         return None
 
     return response.json()
+
+async def _load_json_from_session(session, url):
+    """ Loads data from URL as dictionary from a session using the
+        aiohttp library asynchronously. Intended to be used with
+        asyncio event loop. """
+    # Check input URL before sending request
+    if not _check_url(url):
+        logger.error(f"{url} is invalid for Stats API access.")
+        return {}
+
+    async with session.get(url) as resp:
+        return await resp.json()
+
+async def _load_jsons_async(url_list):
+    """ Loads data from the given URL list asynchronously. Returns
+        a list of dictionaries where each dictionary is expected to
+        be in the same order as the input URL list. """
+    json_data_list = []
+    async with aiohttp.ClientSession() as session:
+        tasks = []
+        for url in url_list:
+            tasks.append(asyncio.create_task(_load_json_from_session(session, url)))
+
+        json_data_list = await asyncio.gather(*tasks)
+    return json_data_list
--- tests/test_statsapi_utils.py
+++ tests/test_statsapi_utils.py
@@ -2,6 +2,7 @@
 import json
 import os
 import requests
+import shutil
 import unittest
 
 SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
@@ -13,7 +14,8 @@ import statsapi_utils
 class TestStatsApiUtils(unittest.TestCase):
     def setUp(self):
         """ Set-up required items. """
-        pass
+        self._test_folder_path = os.path.join(SCRIPT_DIR, "test_statsapi_utils")
+        os.makedirs(self._test_folder_path, exist_ok=True)
 
     def test_load_json_from_url(self):
         """ Test functionality to load stats API data to JSON given a URL. """
@@ -64,70 +66,73 @@ class TestStatsApiUtils(unittest.TestCase):
 
     def test_save_json_from_url(self):
         """ Test functionality to download/save stats API data to JSON given a URL. """
+        test_folder_path = os.path.join(self._test_folder_path, "test_save_json_from_url")
+        os.makedirs(test_folder_path, exist_ok=True)
+
         # Test typical case
         url = "https://statsapi.web.nhl.com/api/v1/teams"
-        out_path = os.path.join(SCRIPT_DIR, "out.json")
+        out_path = os.path.join(test_folder_path, "out.json")
         statsapi_utils.save_json_from_url(url, out_path)
         self.assertTrue(os.path.exists(out_path))
         self._rm_file(out_path)
 
         # Test invalid URL
         url = "https://statsapi.web.nhl.com/ap/v1/teams"
-        out_path = os.path.join(SCRIPT_DIR, "out.json")
+        out_path = os.path.join(test_folder_path, "out.json")
         statsapi_utils.save_json_from_url(url, out_path)
         self.assertFalse(os.path.exists(out_path))
 
         # Test invalid URL
         url = "https://statsapi.web.nhl.com/api/v1/teams/invalid"
-        out_path = os.path.join(SCRIPT_DIR, "out.json")
+        out_path = os.path.join(test_folder_path, "out.json")
         statsapi_utils.save_json_from_url(url, out_path)
         self.assertFalse(os.path.exists(out_path))
 
         # Test invalid URL
         url = "htts://statsapi.web.nhl.com/api/v1/teams"
-        out_path = os.path.join(SCRIPT_DIR, "out.json")
+        out_path = os.path.join(test_folder_path, "out.json")
         statsapi_utils.save_json_from_url(url, out_path)
         self.assertFalse(os.path.exists(out_path))
 
         # Test invalid URL
         url = "https://www.google.com"
-        out_path = os.path.join(SCRIPT_DIR, "out.json")
+        out_path = os.path.join(test_folder_path, "out.json")
         statsapi_utils.save_json_from_url(url, out_path)
         self.assertFalse(os.path.exists(out_path))
 
         # Test invalid URL
         url = 12345
-        out_path = os.path.join(SCRIPT_DIR, "out.json")
+        out_path = os.path.join(test_folder_path, "out.json")
         statsapi_utils.save_json_from_url(url, out_path)
         self.assertFalse(os.path.exists(out_path))
 
         # Test invalid URL
         url = ""
-        out_path = os.path.join(SCRIPT_DIR, "out.json")
+        out_path = os.path.join(test_folder_path, "out.json")
         statsapi_utils.save_json_from_url(url, out_path)
         self.assertFalse(os.path.exists(out_path))
 
         # Test invalid URL
         url = None
-        out_path = os.path.join(SCRIPT_DIR, "out.json")
+        out_path = os.path.join(test_folder_path, "out.json")
         statsapi_utils.save_json_from_url(url, out_path)
         self.assertFalse(os.path.exists(out_path))
 
         # Test invalid output file
         url = "https://statsapi.web.nhl.com/api/v1/teams"
-        out_path = os.path.join(SCRIPT_DIR, "out.jsn")
+        out_path = os.path.join(test_folder_path, "out.jsn")
         statsapi_utils.save_json_from_url(url, out_path)
         self.assertFalse(os.path.exists(out_path))
 
         # Test invalid output file
         url = "https://statsapi.web.nhl.com/api/v1/teams"
-        out_path = os.path.join(SCRIPT_DIR, "out.csv")
+        out_path = os.path.join(test_folder_path, "out.csv")
         statsapi_utils.save_json_from_url(url, out_path)
         self.assertFalse(os.path.exists(out_path))
 
         # Test invalid output file
         url = "https://statsapi.web.nhl.com/api/v1/teams"
-        out_path = os.path.join(SCRIPT_DIR, "out.jsonn")
+        out_path = os.path.join(test_folder_path, "out.jsonn")
         statsapi_utils.save_json_from_url(url, out_path)
         self.assertFalse(os.path.exists(out_path))
 
@@ -135,7 +140,96 @@ class TestStatsApiUtils(unittest.TestCase):
         url = "https://statsapi.web.nhl.com/api/v1/teams"
         out_path = None
         statsapi_utils.save_json_from_url(url, out_path)
-        self.assertFalse(os.path.exists(os.path.join(SCRIPT_DIR, "out.json")))
+        self.assertFalse(os.path.exists(os.path.join(test_folder_path, "out.json")))
+
+    def test_load_jsons_from_urls_async(self):
+        """ Test functionality to load JSONs asynchronously. """
+        # Test typical input
+        # Check for each returned dictionary, check the ID
+        url_list = [f"https://statsapi.web.nhl.com/api/v1/teams/{team_id}"
+                    for team_id in range(1, 6)]
+        actual_data = statsapi_utils.load_jsons_from_urls_async(url_list)
+        for i, json_data in enumerate(actual_data):
+            self.assertEqual(json_data['teams'][0]['id'], i + 1)
+
+        # Test typical input
+        # Check order of returned dictionaries are preserved using async
+        # Retry multiple times to test repeatability
+        url_list = ["https://statsapi.web.nhl.com/api/v1/teams/1",
+                    "https://statsapi.web.nhl.com/api/v1/teams/20",
+                    "https://statsapi.web.nhl.com/api/v1/teams/12",
+                    "https://statsapi.web.nhl.com/api/v1/teams/55",
+                    "https://statsapi.web.nhl.com/api/v1/teams/2"]
+
+        num_tries = 3
+        for i in range(0, num_tries):
+            actual_data = statsapi_utils.load_jsons_from_urls_async(url_list)
+            self.assertEqual(actual_data[0]['teams'][0]['id'], 1)
+            self.assertEqual(actual_data[1]['teams'][0]['id'], 20)
+            self.assertEqual(actual_data[2]['teams'][0]['id'], 12)
+            self.assertEqual(actual_data[3]['teams'][0]['id'], 55)
+            self.assertEqual(actual_data[4]['teams'][0]['id'], 2)
+
+        # Test invalid inputs
+        url_list = ["https://statsapi.web.nhl.com/api/v???/teams/0",
+                    "https://statsapi.web.nhl.com/api/v1/teams/20",
+                    "https://?invalid?url?"]
+        actual_data = statsapi_utils.load_jsons_from_urls_async(url_list)
+        self.assertEqual(actual_data[0], {})
+        self.assertEqual(actual_data[1]['teams'][0]['id'], 20)
+        self.assertEqual(actual_data[2], {})
+
+
+    def test_save_jsons_from_urls_async(self):
+        """ Test functionality to save JSONs asynchronously. """
+        test_folder_path = os.path.join(self._test_folder_path, "test_save_jsons_from_urls_async")
+        os.makedirs(test_folder_path, exist_ok=True)
+
+        # Test typical input
+        # Check for saved files and their corresponding IDs in file
+        input_dicts = [{'url': "https://statsapi.web.nhl.com/api/v1/teams/1", 'out_file_path': os.path.join(test_folder_path, "team1.json")},
+                       {'url': "https://statsapi.web.nhl.com/api/v1/teams/2", 'out_file_path': os.path.join(test_folder_path, "team2.json")},
+                       {'url': "https://statsapi.web.nhl.com/api/v1/teams/3", 'out_file_path': os.path.join(test_folder_path, "team3.json")}]
+
+        num_saved = statsapi_utils.save_jsons_from_urls_async(input_dicts)
+        self.assertTrue(os.path.exists(os.path.join(test_folder_path, "team1.json")))
+        self.assertTrue(os.path.exists(os.path.join(test_folder_path, "team2.json")))
+        self.assertTrue(os.path.exists(os.path.join(test_folder_path, "team3.json")))
+        self.assertEqual(self._load_json_dict(os.path.join(test_folder_path, "team1.json"))['teams'][0]['id'], 1)
+        self.assertEqual(self._load_json_dict(os.path.join(test_folder_path, "team2.json"))['teams'][0]['id'], 2)
+        self.assertEqual(self._load_json_dict(os.path.join(test_folder_path, "team3.json"))['teams'][0]['id'], 3)
+        self.assertEqual(num_saved, 3)
+
+        # Test typical input
+        # Check order of data is preserved using async
+        # Retry multiple times to test repeatability
+        input_dicts = [{'url': "https://statsapi.web.nhl.com/api/v1/teams/3", 'out_file_path': os.path.join(test_folder_path, "team3.json")},
+                       {'url': "https://statsapi.web.nhl.com/api/v1/teams/1", 'out_file_path': os.path.join(test_folder_path, "team1.json")},
+                       {'url': "https://statsapi.web.nhl.com/api/v1/teams/2", 'out_file_path': os.path.join(test_folder_path, "team2.json")}]
+        num_tries = 3
+        for i in range(0, num_tries):
+            num_saved = statsapi_utils.save_jsons_from_urls_async(input_dicts)
+            self.assertTrue(os.path.exists(os.path.join(test_folder_path, "team1.json")))
+            self.assertTrue(os.path.exists(os.path.join(test_folder_path, "team2.json")))
+            self.assertTrue(os.path.exists(os.path.join(test_folder_path, "team3.json")))
+            self.assertEqual(self._load_json_dict(os.path.join(test_folder_path, "team1.json"))['teams'][0]['id'], 1)
+            self.assertEqual(self._load_json_dict(os.path.join(test_folder_path, "team2.json"))['teams'][0]['id'], 2)
+            self.assertEqual(self._load_json_dict(os.path.join(test_folder_path, "team3.json"))['teams'][0]['id'], 3)
+            self.assertEqual(num_saved, 3)
+
+        # Test invalid input
+        input_dicts = [{'url': "https://statsapi.web.nhl.com/api/v???/teams", 'out_file_path': os.path.join(test_folder_path, "team1.json")},
+                       {'url': "https://statsapi.web.nhl.com/api/v1/teams/2", 'out_file_path': os.path.join(test_folder_path, "team2.json")},
+                       {'url': "https://?invalid?url?", 'out_file_path': os.path.join(test_folder_path, "team3.json")}]
+
+        num_saved = statsapi_utils.save_jsons_from_urls_async(input_dicts)
+        self.assertTrue(os.path.exists(os.path.join(test_folder_path, "team1.json")))
+        self.assertTrue(os.path.exists(os.path.join(test_folder_path, "team2.json")))
+        self.assertTrue(os.path.exists(os.path.join(test_folder_path, "team3.json")))
+        self.assertEqual(self._load_json_dict(os.path.join(test_folder_path, "team1.json")), {})
+        self.assertEqual(self._load_json_dict(os.path.join(test_folder_path, "team2.json"))['teams'][0]['id'], 2)
+        self.assertEqual(self._load_json_dict(os.path.join(test_folder_path, "team3.json")), {})
+        self.assertEqual(num_saved, 3)
 
     def test_get_full_url(self):
         """ Test functionality of getting the full statsapi URL link. """
@@ -144,6 +238,10 @@ class TestStatsApiUtils(unittest.TestCase):
         expected_result = statsapi_utils.URL_STRING + suffix
         actual_result = statsapi_utils.get_full_url(suffix)
         self.assertEqual(expected_result, actual_result)
+
+    def _load_json_dict(self, file_path):
+        """ Helper function to load JSON dictionary from file. """
+        return json.load(open(file_path, 'r'))
 
     def _rm_file(self, file):
         """ Helper function to remove a file. Does nothing if it doesn't exist. """
@@ -154,4 +252,4 @@ class TestStatsApiUtils(unittest.TestCase):
 
     def tearDown(self):
         """ Remove any items. """
-        pass
+        shutil.rmtree(self._test_folder_path)
