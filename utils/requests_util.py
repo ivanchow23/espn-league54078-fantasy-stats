82d79373782d6300e18ab387258d582f2f99b7c2
Ivan Chow <ivanchow23@hotmail.com>
2022-03-06 12:14:14 -0700

    [UTILS] Add headers and cookies to requests util
    Added headers and cookies parameters in request utils public APIs.
    Note: These were added on a per-method basis, rather than in the
    constructor because headers and cookie parameters could differ
    depending on which endpoint is used for the same base URL.

--- utils/requests_util.py
+++ utils/requests_util.py
@@ -11,15 +11,15 @@ class RequestsUtil():
     def __init__(self, base_url):
         self._base_url = base_url
 
-    def load_json_from_endpoint(self, endpoint):
+    def load_json_from_endpoint(self, endpoint, headers=None, cookies=None):
         """ Loads JSON data from an endpoint into a dictionary. """
-        return self._load_json(f"{self._base_url}{endpoint}")
+        return self._load_json(f"{self._base_url}{endpoint}", headers, cookies)
 
-    def save_json_from_endpoint(self, endpoint, out_file_path):
+    def save_json_from_endpoint(self, endpoint, out_file_path, headers=None, cookies=None):
         """ Save data from the an endpoint into a JSON file output.
             Returns True if request and data saving is successful. """
         # Load data
-        json_data = self._load_json(f"{self._base_url}{endpoint}")
+        json_data = self._load_json(f"{self._base_url}{endpoint}", headers, cookies)
         if json_data is None:
             return False
 
@@ -28,7 +28,7 @@ class RequestsUtil():
             json.dump(json_data, out_file)
         return True
 
-    def load_jsons_from_endpoints_async(endpoint_list):
+    def load_jsons_from_endpoints_async(endpoint_list, headers=None, cookies=None):
         """ Loads JSON data from the given list of endpoints asynchronously.
             Returns a list of dictionaries. """
         # Set policy on Windows and Python 3.8+ to work around runtime exception:
@@ -39,9 +39,9 @@ class RequestsUtil():
             asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
         url_list = [f"{self._base_url}{endpoint}" for endpoint in endpoint_list]
-        return asyncio.run(_load_jsons_async(url_list))
+        return asyncio.run(_load_jsons_async(url_list, headers, cookies))
 
-    def save_jsons_from_endpoints_async(self, endpoints_file_path_dict_list):
+    def save_jsons_from_endpoints_async(self, endpoints_file_path_dict_list, headers=None, cookies=None):
         """ Saves JSON data from the given endpoints and corresponding file
             output path asynchronously. Input is a list of dictionaries in the
             form: {'endpoint': <endpoint>, 'out_file_path': <output file path>}
@@ -65,23 +65,23 @@ class RequestsUtil():
             asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
         # Run event loop and retrieve list of data after requests are finished
-        json_data_list = asyncio.run(self._load_jsons_async(url_list))
+        json_data_list = asyncio.run(self._load_jsons_async(url_list, headers, cookies))
         for json_data, file_path in zip(json_data_list, file_path_list):
             with open(file_path, 'w') as out_file:
                 json.dump(json_data, out_file)
 
         return len(json_data_list)
 
-    def _load_json(self, url):
+    def _load_json(self, url, headers=None, cookies=None):
         """ Loads data from the URL as a dictionary. """
         # Send request to URL
-        response = requests.get(url)
+        response = requests.get(url, headers=headers, cookies=cookies)
         if response.status_code != 200:
             return None
 
         return response.json()
 
-    async def _load_jsons_async(self, url_list):
+    async def _load_jsons_async(self, url_list, headers=None, cookies=None):
         """ Loads data from the given URL list asynchronously. Returns
             a list of dictionaries where each dictionary is expected to
             be in the same order as the input URL list. """
@@ -89,16 +89,16 @@ class RequestsUtil():
         async with aiohttp.ClientSession() as session:
             tasks = []
             for url in url_list:
-                tasks.append(asyncio.create_task(self._load_json_from_session(session, url)))
+                tasks.append(asyncio.create_task(self._load_json_from_session(session, url, headers, cookies)))
 
             json_data_list = await asyncio.gather(*tasks)
         return json_data_list
 
-    async def _load_json_from_session(self, session, url):
+    async def _load_json_from_session(self, session, url, headers=None, cookies=None):
         """ Loads data from URL as dictionary from a session using the
             aiohttp library asynchronously. Intended to be used with
             asyncio event loop. """
-        async with session.get(url) as resp:
+        async with session.get(url, headers=headers, cookies=cookies) as resp:
             if resp.status != 200:
                 return {}
             return await resp.json()
