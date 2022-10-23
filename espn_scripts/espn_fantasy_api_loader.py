117e6518c985cfc974a082fa0979689ed845a960
Ivan Chow <ivanchow23@hotmail.com>
2022-10-23 01:12:33 -0600

    [ESPN] Update fantasy API loader to use seasons
    To match the downloader scripts that download data into
    season string folders instead of single year folders.

--- espn_scripts/espn_fantasy_api_loader.py
+++ espn_scripts/espn_fantasy_api_loader.py
@@ -58,19 +58,19 @@ class EspnFantasyApiLoader():
     to load data from it. Assumes the following general structure in data folder:
 
         root_folder
-          2019
+          20192020
             - league_info.json
             - scoring_periods
-              -> 2019_scoring_period1.json
-              -> 2019_scoring_period2.json
+              -> 20192020_scoring_period1.json
+              -> 20192020_scoring_period2.json
               -> ...
             - realtime_stats
             - ...
-          2020
+          20202021
             - league_info.json
             - scoring_periods
-              -> 2020_scoring_period1.json
-              -> 2020_scoring_period2.json
+              -> 20202021_scoring_period1.json
+              -> 20202021_scoring_period2.json
               -> ...
             - realtime_stats
             - ...
@@ -79,19 +79,19 @@ class EspnFantasyApiLoader():
         """ Constructor. Takes in path to root data folder. """
         self._root_folder_path = root_folder_path
 
-    def get_members_id_map(self, year_string):
-        """ Returns a dictionary mapping of IDs to members for given year.
+    def get_members_id_map(self, season_string):
+        """ Returns a dictionary mapping of IDs to members for given season.
             Dictionary has the form: {'<id1>': <name>, '<id2>': <name> .. } """
-        league_info_dict = self._load_json(year_string, f"{year_string}_league_info.json")
+        league_info_dict = self._load_json(season_string, f"{season_string}_league_info.json")
         if league_info_dict is None:
             return None
 
         return {m['id']: f"{m['firstName']} {m['lastName']}" for m in league_info_dict['members']}
 
-    def get_applied_stats_map(self, year_string):
+    def get_applied_stats_map(self, season_string):
         """ Returns a dictionary mapping of stats IDs to stats for the stats kept track
-            of a given year. """
-        league_info_dict = self._load_json(year_string, f"{year_string}_league_info.json")
+            of a given season. """
+        league_info_dict = self._load_json(season_string, f"{season_string}_league_info.json")
         if league_info_dict is None:
             return None
 
@@ -101,24 +101,24 @@ class EspnFantasyApiLoader():
 
         return stats_map
 
-    def get_league_info_dict(self, year_string):
-        """ Returns a dictionary of the league information for the given year. """
-        return self._load_json(year_string, f"{year_string}_league_info.json")
+    def get_league_info_dict(self, season_string):
+        """ Returns a dictionary of the league information for the given season. """
+        return self._load_json(season_string, f"{season_string}_league_info.json")
 
-    def get_scoring_period_dict(self, year_string, id):
-        """ Returns a dictionary for the given scoring period of a year. """
-        return self._load_json(year_string, "scoring_periods", f"{year_string}_scoring_period{id}.json")
+    def get_scoring_period_dict(self, season_string, id):
+        """ Returns a dictionary for the given scoring period of a season. """
+        return self._load_json(season_string, "scoring_periods", f"{season_string}_scoring_period{id}.json")
 
-    def _load_json(self, year_string, *args):
-        """ Reads a JSON file as dictionary from given year and arguments.
-            Example: self._load_json(2020, "2020_league_info.json")
-            Example: self._load_json(2020, "scoring_periods", "2020_scoring_period1.json") """
-        file_path = os.path.join(self._root_folder_path, year_string, *args)
+    def _load_json(self, season_string, *args):
+        """ Reads a JSON file as dictionary from given season and arguments.
+            Example: self._load_json(20202021, "20202021_league_info.json")
+            Example: self._load_json(20202021, "scoring_periods", "20202021_scoring_period1.json") """
+        file_path = os.path.join(self._root_folder_path, season_string, *args)
         if not os.path.exists(file_path):
             return None
 
         # Handle special case of data in different format for older seasons
-        if int(year_string) < 2018:
+        if int(season_string) < 2018:
             return json.load(open(file_path, 'r'))[0]
         else:
             return json.load(open(file_path, 'r'))
