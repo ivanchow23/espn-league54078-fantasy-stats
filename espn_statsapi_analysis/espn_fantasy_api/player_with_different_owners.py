169cf6c72c3d282a50a125392b528b5dd54b0e46
Ivan Chow <ivanchow23@hotmail.com>
2023-04-16 00:27:55 -0600

    [ESA] Update GP sum in player with diff owners
    Use sum of GP column in generated dataframe.

--- espn_statsapi_analysis/espn_fantasy_api/player_with_different_owners.py
+++ espn_statsapi_analysis/espn_fantasy_api/player_with_different_owners.py
@@ -8,17 +8,6 @@ from pathlib import Path
 import pandas as pd
 
 # %%
-# Declare helper functions
-def _get_num_games_played(season_player_df):
-    """ Get number of games played given dataframe of a season. """
-    # First, filter out for all n/a entries in applied stats
-    df = season_player_df.dropna(subset=['appliedTotal'])
-
-    # Next, filter out entries that have 0 applied total stats but for
-    # some reason has n/a entries for every other stat
-    df = df[~((df['appliedTotal'] == 0) & (df[['G', 'A', 'PPP', 'SHP', 'GWG', 'HAT', 'W', 'SO']].isna().all(axis=1) == True))]
-    return len(df)
-# %%
 # Configurations
 daily_rosters_df_path = "espn_fantasy_api_daily_rosters_df.csv"
 daily_rosters_df = pd.read_csv(daily_rosters_df_path)
@@ -30,13 +19,13 @@ for season, season_df in daily_rosters_df.groupby('season'):
         # Player was on multiple teams
         num_teams = len(player_df['owner'].unique())
         if num_teams > 1:
-            total_gp = _get_num_games_played(player_df)
+            total_gp = int(player_df['GP'].sum())
             total_pts = int(player_df['appliedTotal'].sum())
             total_pts_per_game = round(total_pts / total_gp, 1) if total_gp != 0 else 0
             print(f"{season}: {player} was on {num_teams} teams. {total_pts} total points in {total_gp} games (pts/game={total_pts_per_game}).")
 
             for owner, df in player_df.groupby('owner'):
-                gp = _get_num_games_played(df)
+                gp = int(df['GP'].sum())
                 pts = int(df['appliedTotal'].sum())
                 pts_per_game = round(pts / gp, 1) if gp != 0 else 0
                 print(f"{player} playing for {owner} had {pts} points in {gp} games (pts/game={pts_per_game}).")
