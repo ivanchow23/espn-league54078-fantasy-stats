e49eaa0e0c84036ab61ea9c1fd1c7c0dc0747c4e
Ivan Chow <ivanchow23@hotmail.com>
2023-12-19 15:11:35 -0700

    [DASH] Add plotly interactive support
    Add plotly.js script to head of HTML to enable plotly's interactive
    graphs. Set figures to not include plotlyjs to reduce file size.

--- espn_html_dashboard_generator/espn_html_dashboard_generator.py
+++ espn_html_dashboard_generator/espn_html_dashboard_generator.py
@@ -18,7 +18,9 @@ class EspnHtmlDashboardGenerator():
     def __init__(self, html_output_path=os.path.join(SCRIPT_DIR, "espn_html_dashboard.html")):
         """ Default constructor. """
         self._html_output_path = html_output_path
-        self._doc = dominate.document(title="ESPN HTML Dashboard")
+
+        # Set-up HTML document
+        self._doc = self._setup_html_doc()
 
     def get_doc(self):
         """ Returns a reference to the HTML document. """
@@ -30,6 +32,16 @@ class EspnHtmlDashboardGenerator():
             html_doc_string = self._doc.render()
             html_doc_encoded = html_doc_string.encode('utf-8')
             html_file.write(html_doc_encoded)
+
+    def _setup_html_doc(self):
+        """ Sets up an HTML document. """
+        doc = dominate.document(title="ESPN HTML Dashboard")
+
+        # Enable plotly interactive graphs
+        with doc.head:
+            script(src="plotly-2.27.0.min.js", charset="utf-8")
+
+        return doc
 
 if __name__ == "__main__":
     dashboard = EspnHtmlDashboardGenerator()
@@ -43,10 +55,10 @@ if __name__ == "__main__":
     dp_norm_first_fig = dp.get_cumulative_points_norm_by_first_plot(key="appliedTotal", season=SEASON)
     pbp_fig = pbp.get_stats_table(season=SEASON)
 
-    dp_html = dp_fig.to_html(include_plotlyjs=True, full_html=False)
-    dp_norm_avg_html = dp_norm_avg_fig.to_html(include_plotlyjs=True, full_html=False)
-    dp_norm_first_html = dp_norm_first_fig.to_html(include_plotlyjs=True, full_html=False)
-    pbp_html = pbp_fig.to_html(include_plotlyjs=True, full_html=False)
+    dp_html = dp_fig.to_html(include_plotlyjs=False, full_html=False)
+    dp_norm_avg_html = dp_norm_avg_fig.to_html(include_plotlyjs=False, full_html=False)
+    dp_norm_first_html = dp_norm_first_fig.to_html(include_plotlyjs=False, full_html=False)
+    pbp_html = pbp_fig.to_html(include_plotlyjs=False, full_html=False)
 
     with doc:
         with div() as d:
