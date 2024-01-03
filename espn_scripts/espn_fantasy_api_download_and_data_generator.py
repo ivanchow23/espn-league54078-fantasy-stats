0178d402fa536dcd85cf14181ff6403b48d8e3d5
Ivan Chow <ivanchow23@hotmail.com>
2024-01-02 21:12:48 -0700

    [ESPN] Add espn_s2 to download/data gen. wrapper
    Added espn_s2 cookie argument to fantasy API download/data generator
    wrapper script to allow external input to download from private league
    seasons.
    
    Note: If adding argument in the .bat file, % symbols must be escaped
    with double %%.

--- espn_scripts/espn_fantasy_api_download_and_data_generator.py
+++ espn_scripts/espn_fantasy_api_download_and_data_generator.py
@@ -2,6 +2,7 @@
 """ Simple wrapper script that calls the ESPN fantasy API downloader
     and data generator together. Meant to be a one click solution to
     generate data easily for analysis. """
+import argparse
 from espn_fantasy_api_daily_rosters_data_generator import EspnFantasyApiDailyRostersDataGenerator
 from espn_fantasy_api_downloader import EspnFantasyApiDownloader
 from pathlib import Path
@@ -14,10 +15,15 @@ end_year = 2024
 league_id = 54078
 
 if __name__ == "__main__":
+    # Parse args
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument("--espn_s2", required=False, default=None, type=str, help="espn_s2 string used for a cookie for ESPN fantasy API requests.")
+    args = arg_parser.parse_args()
+
     # Download
     print("-------------------- Running ESPN fantasy API downloader --------------------")
     for season in range(start_year, end_year + 1):
-        downloader = EspnFantasyApiDownloader(season, league_id)
+        downloader = EspnFantasyApiDownloader(season, league_id, cookies={'espn_s2': args.espn_s2})
         downloader.download_league_info()
         downloader.download_scoring_periods()
 
