17f58cf900a3797e5c1c7346f1265f4245729a3b
Ivan Chow <ivanchow23@hotmail.com>
2025-11-28 20:17:59 -0700

    Clean-up unused scripts
    - Dashboard generator ported to espn-league54078-dashboard repo
      and will be developed/maintained there
    - Data generator scripts are now in a dedicated folder
    - espn-nhlapi draft data combining is not needed now that we
      only source additional player data from ESPN sources

--- espn_fantasy_api_download_and_data_generator.bat
+++
@@ -1,3 +0,0 @@
-:: Double click to download and generate data for ESPN fantasy daily data
-uv run %~dp0\espn_fantasy_api_scripts\espn_fantasy_api_download_and_data_generator.py
-pause
--- espn_html_dashboard_generator.bat
+++
@@ -1,3 +0,0 @@
-:: Double click to generate HTML dashboard
-uv run %~dp0\espn_html_dashboard_generator\espn_html_dashboard_generator.py
-pause
